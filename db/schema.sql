e3edd1bc1ced56e56307be3b9d0cd755c7b7672a
Sam-Cowman <sam.p.cowman@gmail.com>
2024-06-13 17:10:42 -0500

    Updated comments on files

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,23 @@ CREATE DATABASE employees_db;
 
 -- Create the department table
 CREATE TABLE department (
-    id SERIAL PRIMARY KEY,    -- Primary key for the department
-    name VARCHAR(30) UNIQUE NOT NULL   -- Unique and not null department name
+    id SERIAL PRIMARY KEY,   
+    name VARCHAR(30) UNIQUE NOT NULL 
 );
 
 -- Create the role table
 CREATE TABLE role (
-    id SERIAL PRIMARY KEY,    -- Primary key for the role
-    title VARCHAR(30) UNIQUE NOT NULL,  -- Unique and not null role title
-    salary DECIMAL NOT NULL,  -- Not null salary for the role
-    department_id INTEGER REFERENCES department(id)  -- Foreign key referencing the department table
+    id SERIAL PRIMARY KEY,    
+    title VARCHAR(30) UNIQUE NOT NULL,  
+    salary DECIMAL NOT NULL,  
+    department_id INTEGER REFERENCES department(id)  
 );
 
 -- Create the employee table
 CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,    -- Primary key for the employee
-    first_name VARCHAR(30) NOT NULL,   -- Not null first name
-    last_name VARCHAR(30) NOT NULL,    -- Not null last name
-    role_id INTEGER REFERENCES role(id),  -- Foreign key referencing the role table
-    manager_id INTEGER REFERENCES employee(id)  -- Self-referencing foreign key for the manager
+    id SERIAL PRIMARY KEY,  
+    first_name VARCHAR(30) NOT NULL,  
+    last_name VARCHAR(30) NOT NULL,    
+    role_id INTEGER REFERENCES role(id),  
+    manager_id INTEGER REFERENCES employee(id)  
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,5 +13,5 @@ INSERT INTO role (title, salary, department_id) VALUES
 -- Insert data into the employee table
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),          
-('Jane', 'Smith', 2, 1),          
-('Mike', 'Johnson', 3, 1);         
+('Alex', 'Smith', 2, 1),          
+('Mike', 'Peters', 3, 1);         
